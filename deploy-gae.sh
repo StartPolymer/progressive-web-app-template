e594f27e4226cd810249bc7978c0dd60275a4e01
JosefJezek <josef.jezek@inetdata.cz>
2016-06-18 17:14:14 +0200

    Add dev environments to deploy-gae.sh

--- deploy-gae.sh
+++ deploy-gae.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+BUILD_VERSION=bundled
+environment=$1
+
+case $environment in
+  dev) project_id=pwa-demo ;;
+  stage) project_id=pwa-demo ;;
+  prod) project_id=pwa-demo ;;
+  *) project_id=pwa-demo ;;
+esac
+
 rm -rf build
 polymer build
-cp app.yaml build/bundled
-gcloud preview app deploy build/bundled/app.yaml -q --project $1
+cp app.yaml build/$BUILD_VERSION
+gcloud preview app deploy build/$BUILD_VERSION/app.yaml -q --project $project_id
